1825682c516ef94fa1294124f4023a3ce48e0025
Michael Ryan <michaelryan2895@gmail.com>
2021-03-27 19:19:47 -0400

    03

--- src/App.js
+++ src/App.js
@@ -5,23 +5,44 @@ import Main from './main.js';
 import Footer from './footer.js';
 //import HornedBeasts from './Horned-Beasts.js';
 import Data from './data.json';
+import SelectedBeast from './selected-beast';
 
 class App extends React.Component {
   constructor(props){
     super(props);
   this.state = {
     newData: Data,
+    displayModal: false,
+    favoriteBeast: {},
   }
   }
 
+  displayAsModal = (index) => {
+    this.setState ({favoriteBeast: this.state.newData[index], displayModal: true});
+  }
+
+  handleClose =() => {
+    this.setState({displayModal: false});
+  }
+
+
   render() {
     return (
-      <div>
+      <div className="App">
+        <header>
         <Header />
-        <Main beasts={this.state.newData} />
+        <Main beasts={this.state.newData}
+        displayAsModal={this.displayAsModal} 
+        />
+        <SelectedBeast
+        show={this.state.displayModal}
+        handleClose={this.handleClose}
+        favoriteBeast={this.state.favoriteBeast}
+        />
         <Footer />
+        </header>
       </div>
-    )
+    );
   }
 }
 
